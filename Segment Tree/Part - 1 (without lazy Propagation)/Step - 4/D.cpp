ac1cdf9bf916a0ab2f27c004ac6b8f6ee273eb9c
once-ilovesabnaj <161951610+once-ilovesabnaj@users.noreply.github.com>
2025-04-22 12:59:36 +0600

    Number of Different on Segment
    Given an array 𝑎, consisting of small integers (1≤𝑎𝑖≤40). You need to build a data structure that processes two types of queries:
    
    find the number of different elements on a segment,
    change the element of the array.
    Input
    The first line contains two integers 𝑛 and 𝑞, the length of the array and the number of queries, respectively (1≤𝑛,𝑞≤105).
    
    The second line contains 𝑛 numbers 𝑎1, ..., 𝑎𝑛, where 𝑎𝑖 is the initial state of the array (1≤𝑎𝑖≤40).
    
    The following 𝑞 lines describe the queries. Each of these lines has the format "𝑡𝑦𝑝𝑒𝑖 𝑥𝑖 𝑦𝑖".
    
    If 𝑡𝑦𝑝𝑒𝑖=1, then in the 𝑖-th query you need to find the number of different elements on a segment from 𝑥𝑖 to 𝑦𝑖, inclusive (in this case 1≤𝑥𝑖≤𝑦𝑖≤𝑛).
    
    If 𝑡𝑦𝑝𝑒𝑖=2, then the element with the index 𝑥𝑖 is set to 𝑦𝑖 (in this case 1≤𝑥𝑖≤𝑛, 1≤𝑦𝑖≤40).
    
    Output
    For each request of type 1 print the answer to this request on a separate line.
    
    Example
    inputCopy
    7 6
    1 2 3 6 5 4 19
    1 1 3
    1 2 5
    1 2 4
    2 2 8
    1 1 6
    1 1 3
    outputCopy
    3
    4
    3
    6
    3
    
    
    Signed-off-by: once-ilovesabnaj <161951610+once-ilovesabnaj@users.noreply.github.com>

---
+++ Segment Tree/Part - 1 (without lazy Propagation)/Step - 4/D.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+struct Node {
+    vector<int> occ;
+    Node() {
+        occ.assign(41, 0);
+    }
+
+    Node(int x) {
+        occ.assign(41, 0);
+        occ[x]++;
+    }
+
+    Node operator+(const Node &right) {
+        Node res;
+        for (int i = 1; i <= 40; i++) {
+            res.occ[i] = occ[i] + right.occ[i];
+        }
+        return res;
+    }
+};
+struct SegmentTree {
+    vector<Node> sgt;
+    vector<int> a;
+    int n;
+    SegmentTree(int N) {
+        this->n = N;
+        a.assign(N + 1, 0);
+        sgt.resize(N * 4 + 1);
+    }
+    Node combine(Node a, Node b) {
+        return a + b;        // ➜ sum
+    }
+    void build (int node, int l, int r) {
+        if (l == r) {
+            sgt[node] = Node(a[l]);
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        build(node * 2, l, mid);
+        build(node * 2 + 1, mid + 1, r);
+        sgt[node] = combine(sgt[2 * node], sgt[2 * node + 1]);
+    }
+    void update(int node, int l, int r, int idx, int value) {
+        if (l == r) {
+            sgt[node] = Node(value);
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        if(idx <= mid) {
+            update(node * 2, l, mid, idx, value);
+        } else {
+            update(node * 2 + 1, mid + 1, r, idx, value);
+        }
+        sgt[node] = combine(sgt[2 * node], sgt[2 * node + 1]);
+    }
+    Node query(int node, int l, int r, int ql, int qr) {
+        if (l > qr or r < ql) {
+            return Node(); // Edit here
+        }
+        if (l >= ql and r <= qr) {
+            return sgt[node];
+        }
+        int mid = l + (r - l) / 2;
+        Node left = query(node * 2, l, mid, ql, qr);
+        Node right = query(node * 2 + 1, mid + 1, r, ql, qr);
+        return combine(left, right);
+    }
+    void build() { build(1, 1, n); }
+    void update(int idx, int val) { update(1, 1, n, idx, val); }
+    Node query(int l, int r) { return query(1, 1, n, l, r); }
+};
+signed main() {
+    int n, q;
+    cin >> n >> q;
+
+    SegmentTree a(n);
+    for (int i = 1; i <= n; i++) {
+        cin >> a.a[i];
+    }
+    a.build();
+    while (q--) {
+        int c;
+        cin >> c;
+
+        if (c & 1) {
+            int l, r;
+            cin >> l >> r;
+            Node ans = a.query(l, r);
+            int cnt = 0;
+            for (int i = 1; i <= 40; i++) {
+                cnt += ans.occ[i] != 0;
+            }
+            cout << cnt << endl;
+        } else {
+            int idx, val;
+            cin >> idx >> val;
+            a.update(idx, val);
+        }
+    }
+    return 0;
+}
