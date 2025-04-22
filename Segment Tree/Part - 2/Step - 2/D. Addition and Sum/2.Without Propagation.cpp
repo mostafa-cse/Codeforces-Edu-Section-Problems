22570414faf485d619b132f71ed8abc486931907
once-ilovesabnaj <161951610+once-ilovesabnaj@users.noreply.github.com>
2025-04-23 00:44:06 +0600

    Without Propagation
    Signed-off-by: once-ilovesabnaj <161951610+once-ilovesabnaj@users.noreply.github.com>

---
+++ Segment Tree/Part - 2/Step - 2/D. Addition and Sum/2.Without Propagation.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+#define all(x) x.begin(), x.end()
+string cdn[2] = {"YES", "NO"};
+#ifndef ONLINE_JUDGE
+#define dout(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#else
+#define dout(...)
+#endif
+struct segment_tree {
+	int size;
+	vector<int> operation;
+	vector<int> values;
+
+	int NEUTRAL_ELEMENT = 0;
+
+	int modify_op(int a, int b, int len) {
+		return a + b * len;
+	}
+
+	int calc_op(int a, int b) {
+		return a + b;
+	}
+
+	void apply_mod_op(int &a, int b, int len) {
+		a = modify_op(a, b, len);
+	}
+
+	void init(int n) {
+		size = 1;
+		while (size < n) size *= 2;
+		operation.assign(size * 2 + 1, 0);
+		values.assign(size * 2 + 1, 0);
+	}
+
+	void Modify(int node, int l, int r, int ql, int qr, int val) {
+		if (l >= qr or r <= ql) {
+			return;
+		} else if (l >= ql and r <= qr) {
+			apply_mod_op(operation[node], val, 1);
+			apply_mod_op(values[node], val, r - l);
+		} else {
+			int mid = (l + r) / 2;
+			Modify(node * 2 + 1, l, mid, ql, qr, val);
+			Modify(node * 2 + 2, mid, r, ql, qr, val);
+			values[node] = calc_op(values[2 * node + 1], values[2 * node + 2]);
+			apply_mod_op(values[node], operation[node], r - l);
+		}
+	}
+
+	int query(int node, int l, int r, int ql, int qr) {
+		if (l >= qr or r <= ql) {
+			return NEUTRAL_ELEMENT;
+		} else if (l >= ql and r <= qr) {
+			return values[node];
+		} else {
+			int mid = (l + r) / 2;
+			int left = query(node * 2 + 1, l, mid, ql, qr);
+			int right = query(node * 2 + 2, mid, r, ql, qr);
+			int ans = calc_op(left, right);
+			apply_mod_op(ans, operation[node],min(qr, r) - max(ql, l));
+			return ans;
+		}
+	}
+};
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);  cout.tie(0);
+    int n, q;
+    cin >> n >> q;
+
+    segment_tree a;
+    a.init(n);
+
+    while (q--) {
+    	int op, l, r, v;
+    	cin >> op >> l >> r;
+
+    	if (op & 1) {
+    		cin >> v;
+    		a.Modify(0, 0, n, l, r, v);
+    	} else {
+    		cout << a.query(0, 0, n, l, r) << endl;
+    	}
+    }
+    return 0;
+}
