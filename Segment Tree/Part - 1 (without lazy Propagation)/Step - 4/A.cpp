41fdef3847faf5890a375ae4ab82b742b63c592d
once-ilovesabnaj <161951610+once-ilovesabnaj@users.noreply.github.com>
2025-04-22 12:54:03 +0600

    Sign alternation
    Implement data structure of 𝑛 elements 𝑎1,𝑎2…𝑎𝑛, with the following operations:
    
    assign the element 𝑎𝑖 value 𝑗;
    find alternating sign sum in the range from 𝑙 to 𝑟 inclusive (𝑎𝑙−𝑎𝑙+1+𝑎𝑙+2−…±𝑎𝑟).
    Input
    The first line of the input file contains a natural number 𝑛 (1≤𝑛≤105) — the length of the array. The second line contains the initial values of the elements (non-negative integers not exceeding 104).
    
    The third line contains a positive integer 𝑚 (1≤𝑚≤105) — the number of operations. The following 𝑚 lines contain operations:
    
    the operation of the first type is given by three numbers 0 i j (1≤𝑖≤𝑛, 1≤𝑗≤104).
    an operation of the second type is given by three numbers 1 l r (1≤𝑙≤𝑟≤𝑛).
    Output
    For each operation of the second type, print on a separate line the corresponding sign alternating sum.
    
    Example
    inputCopy
    3
    1 2 3
    5
    1 1 2
    1 1 3
    1 2 3
    0 2 1
    1 1 3
    outputCopy
    -1
    2
    -1
    3
    
    
    Signed-off-by: once-ilovesabnaj <161951610+once-ilovesabnaj@users.noreply.github.com>

---
+++ Segment Tree/Part - 1 (without lazy Propagation)/Step - 4/A.cpp
@@ -0,0 +1,80 @@
+using namespace __gnu_pbds;
+#define int long long
+using namespace std;
+
+struct SegmentTree {
+    vector<int> a, sgt;
+    SegmentTree(int N) {
+        a.assign(N + 1, 0);
+        sgt.assign(N * 4 + 1, 0);
+    }
+    void merge(int node) {
+        sgt[node] = sgt[node * 2] + sgt[node * 2 + 1];      // Sum
+    }
+    int Ans(int left, int right) {
+        return left + right;
+    }
+    void build (int node, int l, int r) {
+        if (l == r) {
+            sgt[node] = a[l] * ((l & 1) ? -1 : 1);
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        build(node * 2, l, mid);
+        build(node * 2 + 1, mid + 1, r);
+        merge(node);
+    }
+    void update(int node, int l, int r, int idx, int value) {
+        if (l == r) {
+            sgt[node] = value * ((idx & 1) ? -1 : 1);
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        if(idx <= mid) {
+            update(node * 2, l, mid, idx, value);
+        } else {
+            update(node * 2 + 1, mid + 1, r, idx, value);
+        }
+        merge(node);
+    }
+    int query(int node, int l, int r, int ql, int qr) {
+        if (l > qr or r < ql) {
+            return 0; // Edit here
+        }
+        if (l >= ql and r <= qr) {
+            return sgt[node];
+        }
+        int mid = l + (r - l) / 2;
+        int left = query(node * 2, l, mid, ql, qr);
+        int right = query(node * 2 + 1, mid + 1, r, ql, qr);
+        return Ans(left, right);
+    }
+};
+signed main() {
+    int n;
+    cin >> n;
+    SegmentTree a(n);
+    for (int i = 1; i <= n; i++) {
+        cin >> a.a[i];
+    }
+    a.build(1, 1, n);
+    
+    int m;
+    cin >> m;
+    while (m--) {
+        int c;
+        cin >> c;
+        if (!c) {
+            int i, j;
+            cin >> i >> j;
+            a.update(1, 1, n, i, j);
+        } else {
+            int l, r;
+            cin >> l >> r;
+
+            int ans = a.query(1, 1, n, l, r);
+            cout << ans * (l & 1 ? -1 : 1) << endl;
+        }
+    }
+    return 0;
+}
